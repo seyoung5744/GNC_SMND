f6464668d808cc8797e67f3732b249c1be9faf8b
wonseyoung <seyoung574458@gmail.com>
2021-10-01 17:09:49 +0900

    refactor : csv to image

--- prpd_to_graph.py
+++ prpd_to_graph.py
@@ -4,25 +4,27 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 prpd = pd.read_csv("./SMND_345kV_EBG_A_S_0A_62_20190515113600.dat[PRPD변환].csv", names=range(0,256)) # surface
-
-prpd_numpy = prpd.to_numpy()
-prpd_numpy = np.array(prpd_numpy, dtype=np.float32)
-prpd_numpy[np.where(prpd_numpy== 0) ] = np.nan
-
 # custom cmap => https://matplotlib.org/3.1.0/tutorials/colors/colormap-manipulation.html
-color_num = 10
-viridis = plt.cm.get_cmap('YlOrRd', color_num)
-newcolors = viridis(np.linspace(0, 1, color_num))
 
-newcolors[-1:, :] = np.array([1/256, 1/256, 1/256, 1]) # black
+def prpd2image(prpd):
+    prpd_numpy = prpd.to_numpy()
+    prpd_numpy += 1
+    prpd_numpy[np.where(prpd_numpy== 1) ] = 0
 
-newcmp = ListedColormap(np.flipud(newcolors))
+    color_num = np.max(prpd_numpy)
+    viridis = plt.cm.get_cmap('YlOrRd', color_num)
+    newcolors = viridis(np.linspace(0, 1, color_num))
 
-plt.figure(figsize=(7,5))
+    newcolors[-1:, :] = np.array([256/256, 256/256, 256/256, 1]) # white
+    newcolors[-10:-1, :] = np.array([1/256, 1/256, 1/256, 1]) # black
+    newcmp = ListedColormap(np.flipud(newcolors))
+
+    plt.figure(figsize=(10,7))
 
 
-plt.imshow(prpd_numpy, cmap=newcmp)
-# plt.clim(-10,np.max(prpd_numpy))
-plt.colorbar()
-plt.savefig("./test_image.jpg")
-plt.show()
+    plt.imshow(prpd_numpy, cmap=newcmp)
+
+    plt.colorbar()
+    plt.clim(-1, np.max(prpd_numpy))
+    plt.savefig("./test_image.jpg")
+    plt.show()
--- psa_to_graph.py
+++ psa_to_graph.py
@@ -4,25 +4,28 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 psa = pd.read_csv("./SMND_345kV_EBG_A_S_0A_62_20190515110800.dat[PSA변환].csv", names=range(0,256)) # surface
-psa_numpy = psa.to_numpy(dtype=np.float32)
-psa_numpy[np.where(psa_numpy== 0) ] = -1
 
-# custom cmap => https://matplotlib.org/3.1.0/tutorials/colors/colormap-manipulation.html
-color_num = 10
-viridis = plt.cm.get_cmap('YlOrRd', color_num)
-newcolors = viridis(np.linspace(0, 1, color_num))
+def psa2image(psa):
+    psa_numpy = psa.to_numpy()
+    psa_numpy += 1
+    psa_numpy[np.where(psa_numpy== 1) ] = 0
 
-newcolors[-1:, :] = np.array([256/256, 256/256, 256/256, 1]) # white
-newcolors[-2:-1, :] = np.array([1/256, 1/256, 1/256, 1]) # black
+    # custom cmap => https://matplotlib.org/3.1.0/tutorials/colors/colormap-manipulation.html
+    color_num = np.max(psa_numpy)
+    viridis = plt.cm.get_cmap('YlOrRd', color_num)
+    newcolors = viridis(np.linspace(0, 1, color_num))
 
-newcmp = ListedColormap(np.flipud(newcolors))
+    # newcolors[-1:, :] = np.array([1/256, 1/256, 1/256, 1]) # black
+    newcolors[-1:, :] = np.array([256/256, 256/256, 256/256, 1]) # white
+    newcolors[-2:-1, :] = np.array([1/256, 1/256, 1/256, 1]) # black
 
-plt.figure(figsize=(10,10))
+    newcmp = ListedColormap(np.flipud(newcolors))
 
-plt.imshow(psa_numpy, cmap=newcmp)
+    plt.figure(figsize=(10,7))
 
-# plt.imshow(psa)
-plt.clim(np.min(psa_numpy),np.max(psa_numpy))
-plt.colorbar()
-plt.savefig("./test_image.jpg")
-plt.show()
+    plt.imshow(psa_numpy, cmap=newcmp)
+
+    plt.clim(-1, np.max(psa_numpy))
+    plt.colorbar()
+    plt.savefig("./test_image.jpg")
+    plt.show()
