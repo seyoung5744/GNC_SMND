5050499df86083d7820faa8c4bd63362d3df18f8
wonseyoung <seyoung574458@gmail.com>
2022-01-27 13:46:27 +0900

    UPDATE prps to prpd v2

--- prps_to_prpd.py
+++ prps_to_prpd.py
@@ -5,45 +5,33 @@ import numpy as np
 
 
 sub_folder_name = ""
-convert_folder_dir = "./data/05. 표준데이터(PRPD)/"
+CONVERTED_FOLDER_DIR = "./data/05. 표준데이터(PRPD)/"
+ORIGIN_FOLDER_DIR = "./data/01. 표준데이터(PRPS)/"
 
+root_labels = [label for label in os.listdir(ORIGIN_FOLDER_DIR)] # ['03. Noise (54)', '00. Void (204)', '02. Surface (144)', '01. Corona (81)']
 
-for root, subdirs, files in os.walk("./data/01. 표준데이터(PRPS)"):
-    print(root)
-    if len(root.split("\\")) != 1:
-        sub_folder_name = root.split("\\")[1]
-    else:
-        continue
-
-    #* 폴더 라벨링을 위한 split
-    labeling = sub_folder_name.split(" ")[1]
-
-    added_folder_name = convert_folder_dir+labeling #* ./data/05. 표준데이터(PRPD)/Void ... ./data/05. 표준데이터(PRPD)/Corona
+# Create save folder
+for label in root_labels:
+    added_folder_name = CONVERTED_FOLDER_DIR+label #* ./data/05. 표준데이터(PRPD)/Void ... ./data/05. 표준데이터(PRPD)/Corona
     if not os.path.exists(added_folder_name):
         os.makedirs(added_folder_name)
-    
-
-    print(labeling)
-    file_list = os.listdir(root)
-    # print(file_list)
-    print(file_list[0])
-
-    print(file_list[0].split("[")[0] + "[PRPD변환].csv")
+        
+# Preprocessing prps to prpd    
+for label in root_labels:
+    file_list = os.listdir(ORIGIN_FOLDER_DIR + label)
 
     for i in range(len(file_list)):
-        # print(root  + "\\" +file_list[i])
-
-        prps = pd.read_csv(root + "\\" + file_list[i], names=range(0,256))
-
-        box = np.full((256,256), 0)
+        prps = pd.read_csv(ORIGIN_FOLDER_DIR + label + "/" + file_list[i], names=range(0,256))
         
-        for j in range(0, len(prps)):
-            for idx, data in enumerate(prps.loc[j]):
-                box[idx][data] += 1
+        box = np.full((256,256), 0)
+        for row, data_series in prps.iteritems():
+            datas = data_series.value_counts()
+            for col, data in datas.iteritems():
+                box[row,col] = data
+
 
         data_df = pd.DataFrame(box, index = range(0, 256), columns=range(0, 256))
-        data_df = data_df.transpose()
+        data_df = data_df.T
         data_df = data_df.sort_index(ascending=False)
         
-        data_df.to_csv(convert_folder_dir + labeling + "/" + file_list[i].split("[")[0] + "[PRPD변환].csv"  , index = False, header=False,  mode="w")
-    
+        data_df.to_csv(CONVERTED_FOLDER_DIR + label + "/" + file_list[i].split("[")[0] + "[PRPD변환].csv"  , index = False, header=False,  mode="w")
